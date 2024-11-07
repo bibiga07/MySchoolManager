842b06e20e02435c57dfca529d785eb457039195
Kim ju hwan <minecraft_bos@naver.com>
2024-11-07 11:53:56 +0900

    project start

---
+++ MySchoolManager/MySchoolManager/MySchoolManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySchoolManagerApp.swift
+//  MySchoolManager
+//
+//  Created by 김주환 on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MySchoolManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MySchoolManager/MySchoolManagerTests/MySchoolManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MySchoolManagerTests.swift
+//  MySchoolManagerTests
+//
+//  Created by 김주환 on 11/7/24.
+//
+
+import Testing
+@testable import MySchoolManager
+
+struct MySchoolManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
