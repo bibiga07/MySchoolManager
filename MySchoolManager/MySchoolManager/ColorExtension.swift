3cc1e3205632f71796982b8e590dc3106c58802e
Kim ju hwan <minecraft_bos@naver.com>
2024-11-07 12:32:11 +0900

    :sparkles: Color Set

---
+++ MySchoolManager/MySchoolManager/ColorExtension.swift
@@ -0,0 +1,14 @@
+//
+//  ColorExtension.swift
+//  MySchoolManager
+//
+//  Created by 김주환 on 11/7/24.
+//
+
+import SwiftUI
+
+extension Color {
+    static let primarycolor = Color("cPrimary")
+    static let secondarycolor = Color("cSecondary")
+    static let thirdcolor = Color("cThird")
+}
