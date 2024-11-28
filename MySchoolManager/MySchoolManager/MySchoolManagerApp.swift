2b98d62076f4e78683f799bd2458c4e64aeb2bba
Kim ju hwan <minecraft_bos@naver.com>
2024-11-28 14:58:07 +0900

    :sparkles: Auto Login

--- MySchoolManager/MySchoolManager/MySchoolManagerApp.swift
+++ MySchoolManager/MySchoolManager/MySchoolManagerApp.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 @main
 struct MySchoolManagerApp: App {
+    @State private var selectedSchoolName: String? = nil
+
     var body: some Scene {
         WindowGroup {
-            SelectView()
+            if let schoolName = selectedSchoolName, !schoolName.isEmpty {
+                // schoolName이 비어 있지 않으면 HomeView로 이동
+                CustomTabBarNavigationView()
+            } else {
+                // schoolName이 비어 있으면 SelectView로 이동
+                SelectView()
+                    .onAppear {
+                        // 앱 시작 시 UserDefaults에서 selectedSchoolName 값 가져오기
+                        selectedSchoolName = UserDefaults.standard.string(forKey: "selectedSchoolName")
+                    }
+            }
         }
     }
 }
